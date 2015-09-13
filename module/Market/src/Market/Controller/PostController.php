9381c015c5a62ddde207e8974a3a58994614b260
David Petro <david.abraao.petro@gmail.com>
2015-09-13 17:48:36 -0300

    M9EX1

--- module/Market/src/Market/Controller/PostController.php
+++ module/Market/src/Market/Controller/PostController.php
@@ -7,16 +7,37 @@ use Zend\View\Model\ViewModel;
 class PostController extends AbstractActionController
 {
     public $categories;
+    public $postForm;
 
     public function setCategories($categories)
     {
         $this->categories = $categories;
     }
 
+    public function setPostForm($postForm)
+    {
+        $this->postForm = $postForm;
+    }
+
     public function indexAction()
     {
-        $viewModel = new ViewModel(array("categories" => $this->categories));
-        $viewModel->setTemplate("market/post/invalid.phtml");
+        $data = $this->params()->fromPost();
+        $viewModel = new ViewModel(array("postForm"=> $this->postForm, 'data' => $data));
+        $viewModel->setTemplate("market/post/index.phtml");
+
+        if($this->getRequest()->isPost()){
+            $this->postForm->setData($data);
+            if($this->postForm->isValid()){
+                $this->flashMessenger()->addMessage("Thanks for posting!");
+                $this->redirect()->toRoute("home");
+            } else {
+
+                $invalidView = new ViewModel();
+                $invalidView->setTemplate("market/post/invalid.phtml");
+                $invalidView->addChild($viewModel, "main");
+                return $invalidView;
+            }
+        }
 
         return $viewModel;
     }
--- module/Market/src/Market/Factory/PostControllerFactory.php
+++ module/Market/src/Market/Factory/PostControllerFactory.php
@@ -12,9 +12,15 @@ class PostControllerFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $categories = $serviceLocator->getServiceLocator()->get("categories");
+        $allService = $serviceLocator->getServiceLocator();
+        $sm = $allService->get("ServiceManager");
+
+        $categories = $sm->get("categories");
+
         $postController = new \Market\Controller\PostController();
         $postController->setCategories($categories);
+        $postController->setPostForm($sm->get("market-post-form"));
+
         return $postController;
     }
 }
